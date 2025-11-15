5e73667b4f902ab9574c6e90b574c5a86b331f32
Suryamukhi Venigalla <suryav27@cs.utexas.edu>
2025-11-14 22:22:36 -0600

    slight changes fixed now

--- Scheduler.hpp
+++ Scheduler.hpp
@@ -9,10 +9,7 @@
 #include "SimTypes.h"              // Defines VMId_t, MachineId_t, etc.
 
 
-// -----------------------------------------------------------
 // Records of created VMs
-// -----------------------------------------------------------
-
 struct VMRec {
     VMId_t      id;
     MachineId_t host;
@@ -22,10 +19,7 @@ struct VMRec {
     unsigned    running_tasks;
 };
 
-// -----------------------------------------------------------
 // Pending Attach for sleeping hosts waking up
-// -----------------------------------------------------------
-
 struct PendingAttach {
     VMId_t      vm;
     MachineId_t host;
@@ -33,10 +27,7 @@ struct PendingAttach {
     Priority_t  pr;
 };
 
-// -----------------------------------------------------------
 // Scheduler Class
-// -----------------------------------------------------------
-
 class Scheduler {
 public:
     Scheduler() = default;
@@ -47,7 +38,6 @@ public:
     void MigrationComplete(Time_t time, VMId_t vm);
     void PeriodicCheck(Time_t time);
 
-    // UPDATED SIGNATURE (must match Scheduler.cpp)
     void HandleWakeComplete(Time_t now, MachineId_t machine_id);
 
     void Shutdown(Time_t time);
